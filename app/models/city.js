c2d8c6b562ad96123a650a8b47dec3f72a3b559f
Josh Paul <softwarecreator@icloud.com>
2016-12-04 12:41:18 -0800

    Add relationship attributes to city and neighborhood.

--- app/models/city.js
+++ app/models/city.js
@@ -2,5 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-  description: DS.attr('text')
+  description: DS.attr('text'),
+  neighborhoods: DS.hasMany('neighborhood')
 });
